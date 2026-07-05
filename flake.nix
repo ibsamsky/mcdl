410991dead52ac67224c00d21978b6f15ec67f64
Isaac Samsky <github+ibsamsky@cark.moe>
2026-07-04 23:06:53 -0400

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,73 @@
+# h/t https://github.com/niri-wm/niri/blob/main/flake.nix
+{
+  description = "mcdl: Minecraft server management tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }@inputs:
+    let
+      mcdl-package =
+        {
+          pkg-config,
+          rustPlatform,
+        }:
+        rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "mcdl";
+          version = self.shortRev or self.dirtyShortRev or "unknown";
+
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = [ pkg-config ];
+
+          useNextest = true;
+          checkFlags = [
+            # filter out tests that require network access or other external dependencies
+            "types::"
+            "utils::macros::"
+            # install_jre and utils::net::* require network. mcdl::bin_tests tries to instantiate a
+            # reqwest client which fails to load CA certs.
+            # TODO: vm test?
+          ];
+        });
+
+      inherit (nixpkgs) lib;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = lib.intersectLists lib.systems.flakeExposed lib.platforms.linux;
+
+      perSystem =
+        { self', pkgs, ... }:
+        {
+          checks = {
+            inherit (self'.packages) mcdl-debug;
+          };
+
+          packages =
+            let
+              mcdl = pkgs.callPackage mcdl-package { };
+            in
+            {
+              inherit mcdl;
+
+              mcdl-debug = mcdl.overrideAttrs (final: prev: {
+                pname = "${prev.pname}-debug";
+
+                cargoBuildType = "debug";
+                cargoCheckType = final.cargoBuildType;
+              });
+
+              default = mcdl;
+            };
+        };
+    };
+}
